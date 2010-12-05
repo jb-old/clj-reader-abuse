b4b8a700e9127f35e3d0273d666c143b286c6623
Jeremy Banks <jeremy@jeremybanks.com>
2010-12-05 10:36:03 -0500

    should be almost working markov?

--- src/reader_abuse/trying.clj
+++ src/reader_abuse/trying.clj
@@ -31,47 +31,69 @@ defn take-all [n s]
   "(take n s) or nil if #[n < (count s)]."
   
   let [v (vec (take n s))]
-    if #[(count v) < n]
+    if #[(count v) = n]
       v
 
 defn chains [n s]
   "Returns a lazy sequence of all length n subsequences of s."
   
-    if (first s)
+  let [link (take-all n s)]
+    if link
       lazy-seq
-        take-all n (first s)
-        chains n (next s)
+        cons
+          link
+          chains n (rest s)
 
 defn vec-split-last [v]
   let [n (count v)]
     list
       subvec v 0 #[n - 1]
-      nth #[n - 1] v
+      nth v #[n - 1]
 
-defn following-frequencies
+defn following-occurrences
+  "Given a sequence of 2-item sequences and returns a map from items that
+  occur first to a map from the items that appear after them to the number of
+  times they do so."
+  
   [s]
-    following-frequencies s {}
+    following-occurrences s {}
   
   [s m]
-    let [[key value] (first s)
-         new-frequencies (merge-with + {  } )
+    let [[tail head] (first s)
+          new-counts (merge-with #(merge-with + %1 %2)
+                                 m { tail { head 1 } })]
+        if (seq (rest s))
+          recur (rest s) new-counts
+          new-counts
+
+defn map-keys [f m]
+  "Maps a function over the keys of a map."
   
+  into {}
+    map
+      fn [[key_ value]]
+        list (f key_) value
+      m
 
+defn map-values [f m]
+  "Maps a function over the values of a map."
+  
+  into {}
+    map
+      fn [[key_ value]]
+        list key_ (f value)
+      m
 
-defn get-one []
-  weighted-sample
-    normalize-weights
-      list
-        { :weight 5 :value "A" }
-        { :weight 2 :value "B" }
+defn following-weights [s]
+  map-values
+    fn [[value occurrences]]
+      {:value value :weight occurrences}
+    following-occurrences s
 
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
-println (get-one)
+println
+  "<pre>"
+  following-occurrences
+    map
+      vec-split-last
+      chains 2 "aabcdadbc"
+  "</pre>"
